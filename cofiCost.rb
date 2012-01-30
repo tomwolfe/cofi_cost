9a0a7cc599f094665968124a80e8bf7c20c43563
Thomas Wolfe <tomwolfe@gmail.com>
2012-01-30 00:56:43 -0600

    removed camelCase and replace with underscores for readability and convention. added the first unit test which still needs work. allows theta and features to be specified on initialization for testing, but nil for default. added a new method to return all of the predictions after gradient descent.

--- cofiCost.rb
+++ cofiCost.rb
@@ -4,83 +4,82 @@ require 'narray'
 
 include GSL::MultiMin
 
-class ConfiCost
+class CofiCost
 
-	attr_accessor :ratings, :trackList, :numFeatures, :cost, :lambda # trackList: activerecord lookup of all track_ids
-	attr_reader :booleanRated, :numTracks, :numUsers, :features, :theta, :ratingsMean, :ratingsNorm
+	attr_accessor :ratings, :num_features, :cost, :lambda
+	attr_reader :boolean_rated, :num_tracks, :num_users, :features, :theta, :ratings_mean, :ratings_norm, :predictions
 	
-	def initialize(ratings, trackList, numFeatures, lambda)
+	def initialize(ratings, num_features, lambda, features = nil, theta = nil)
 		@ratings = ratings.to_f	# make sure it's a float for correct normalization
-		@trackList = trackList
-		@numFeatures = numFeatures
+		@num_features = num_features
 		@cost = 0
-		@booleanRated = @ratings > 0
-		@numTracks = @ratings.shape[1] # @ratings is users x tracks
-		@numUsers = @ratings.shape[0]
+		@boolean_rated = @ratings > 0 # return 0 for all rated and 1 for all unrated
+		@boolean_unrated = @boolean_rated.eq 0 # return 1 for all unrated and 0 for all unrated
+		@num_tracks = @ratings.shape[1] # @ratings is users x tracks
+		@num_users = @ratings.shape[0]
 		# set initial parameters
-		#@features = NArray.float(@numFeatures, @numTracks).randomn
-		#@theta = NArray.float(@numFeatures, @numUsers).randomn
-		@features = NArray[[0.139489,1.804804],[-0.501808,1.050885],[0.354079,-0.518884],[-0.015370,0.096253],[1.147623,-0.745562]]
-		@theta = NArray[[-0.079641,1.211386],[-0.130688,0.444762],[-0.789258,1.222232],[0.212132,-1.174545]]
-		
-		@ratingsMean = NArray.float(1, @numTracks).fill(0.0)
-		@ratingsNorm = NArray.float(@numUsers, @numTracks).fill(0.0)
-		@ratingsMean, @ratingsNorm = normalizeRatings
+		# allow theta/features to be set for testing
+		if features.nil? then @features = NArray.float(@num_features, @num_tracks).randomn else @features = features end
+		if theta.nil? then @theta = NArray.float(@num_features, @num_users).randomn else @theta = theta end
+		@ratings_mean = NArray.float(1, @num_tracks).fill(0.0)
+		@ratings_norm = NArray.float(@num_users, @num_tracks).fill(0.0)
+		@ratings_mean, @ratings_norm = normalize_ratings
 		@lambda = lambda
+		@predictions = nil
 	end
 	
-	def normalizeRatings
-		for i in 0..@numTracks-1 # sadly, @numTracks.each_index does not work with NArray yet
-			trackRating = @ratings[true,i] # get all user ratings for track i (including unrated)
-			booleanTrackRating = booleanRated[true,i] # get all user ratings that exist for track i
-		    	@ratingsMean[i] = trackRating[booleanTrackRating].mean
+	def normalize_ratings
+		for i in 0..@num_tracks-1 # sadly, @num_tracks.each_index does not work with NArray yet
+			track_rating = @ratings[true,i] # get all user ratings for track i (including unrated)
+			boolean_track_rating = boolean_rated[true,i] # get all user ratings that exist for track i
+		    	@ratings_mean[i] = track_rating[boolean_track_rating].mean
 		    	
-		    	trackNorm = @ratingsNorm[true,i]
-		    	trackNorm[booleanTrackRating] = trackRating[booleanTrackRating] - @ratingsMean[i]
-		    	@ratingsNorm[true,i] = trackNorm
+		    	track_norm = @ratings_norm[true,i]
+		    	track_norm[boolean_track_rating] = track_rating[boolean_track_rating] - @ratings_mean[i]
+		    	@ratings_norm[true,i] = track_norm
 		end
-		return @ratingsMean, @ratingsNorm
+		return @ratings_mean, @ratings_norm
 	end
 	
-	def unrollParams(v)
+	def unroll_params(v)
 		v = v.to_na
 		theta = v.slice(0..@theta.size-1).reshape(@theta.shape[0],true)
 		features = v.slice(@theta.size..-1).reshape(@features.shape[0],true)
 		return theta, features
 	end
 	
-	def partialCostCalc(theta,features)
-		(NArray.ref(NMatrix.ref(features) * NMatrix.ref(theta.transpose(1,0))) - @ratingsNorm) * @booleanRated
+	def partial_cost_calc(theta,features)
+		(NArray.ref(NMatrix.ref(features) * NMatrix.ref(theta.transpose(1,0))) - @ratings_norm) * @boolean_rated
 	end
 	
-	def rollUpThetaFeatures
+	def roll_up_theta_and_features
 		# roll up theta and features together
 		# (oddly, NArray objects created don't seem to recognize the hcat method
 		# 	added to the open class NArray
 		#	x = GSL:: Vector.alloc(@theta.reshape(true).hcat(@features.reshape(true)))
 		#		will fail)
 		#		I don't understand why this is/how to fix it.
-		thetaReshaped = @theta.reshape(true)
-		featuresReshaped = @features.reshape(true)
-		rolled = NArray.hcat(thetaReshaped,featuresReshaped)
+		theta_reshaped = @theta.reshape(true)
+		features_reshaped = @features.reshape(true)
+		rolled = NArray.hcat(theta_reshaped,features_reshaped)
 		GSL:: Vector.alloc(rolled) # starting point
 	end
 	
-	def unrollParamsInitShape(x)
-		theta, features = unrollParams(x)
+	def unroll_params_init_shape(x)
+		theta, features = unroll_params(x)
 		@theta = theta.reshape(@theta.shape[0],true)
 		@features = features.reshape(@features.shape[0],true)
 	end
 	
-	def minCost
+	def min_cost
 		cost_f = Proc.new { |v|
-			theta, features = unrollParams(v)
+			theta, features = unroll_params(v)
 			# In octave:
 			# 1/2 * sum(sum(((X * Theta.transpose - Y).*R).^2)) + lambda/2 * sum(sum((Theta).^2)) + lambda/2 * sum(sum((X).^2))
-			(partialCostCalc(theta,features)**2).sum + @lambda/2 * (features**2).sum
+			(partial_cost_calc(theta,features)**2).sum + @lambda/2 * (features**2).sum
 		}
 		cost_df = Proc.new { |v, df|
-			theta, features = unrollParams(v)
+			theta, features = unroll_params(v)
 			# In octave:
 			# xgrad = ((X * Theta.transpose - Y).* R) * Theta + lambda * X # X_grad
 			# thetagrad = ((X * Theta.transpose - Y).* R).transpose * X + lambda * Theta
@@ -88,15 +87,15 @@ class ConfiCost
 			# I realize this is a hack. I'm not totally sure why or how but just setting
 			# df = NArray.hcat(dfzero,dfone) results in no steps being made in gradient descent.
 			# ideas/suggestions welcome :)
-			dfzero = (NArray.ref(NMatrix.ref(partialCostCalc(theta,features)) * NMatrix.ref(theta)) + @lambda * features).flatten
-			dfone = (NArray.ref(NMatrix.ref((partialCostCalc(theta,features)).transpose(1,0)) * NMatrix.ref(features)) + @lambda * theta).flatten
+			dfzero = (NArray.ref(NMatrix.ref(partial_cost_calc(theta,features)) * NMatrix.ref(theta)) + @lambda * features).flatten
+			dfone = (NArray.ref(NMatrix.ref((partial_cost_calc(theta,features)).transpose(1,0)) * NMatrix.ref(features)) + @lambda * theta).flatten
 			dfcomp = NArray.hcat(dfzero,dfone)
 			for i in 0..dfcomp.size-1	# again .each_index does not yet work with NArray
 				df[i] = dfcomp[i]
 			end
 		}
 		
-		x = rollUpThetaFeatures
+		x = roll_up_theta_and_features
 		cost_func = Function_fdf.alloc(cost_f, cost_df, x.size)
 
 		# TODO: figure out which algorithm to use
@@ -117,11 +116,13 @@ class ConfiCost
 			printf("%5d %.5f %.5f %10.5f\n", iter, x[0], x[1], f)
 		end while status == GSL::CONTINUE and iter < 10
 		
-		unrollParamsInitShape(x)
+		unroll_params_init_shape(x)
+		@cost = f
+		@predictions = calc_predictions
 	end
 	
-	def predictions
-		NArray.ref(NMatrix.ref(@features) * NMatrix.ref(@theta.transpose(1,0))) + @ratingsMean
+	def calc_predictions
+		NArray.ref(NMatrix.ref(@features) * NMatrix.ref(@theta.transpose(1,0))) + @ratings_mean
 	end
 
 end
@@ -151,16 +152,16 @@ class NArray
 	end
 end
 
-#ratings = NArray.float(4,5).indgen(0,2)
-ratings = NArray[[5.0,4.0,0.0,0.0],[3.0,0.0,0.0,0.0],[4.0,0.0,0.0,0.0],[3.0,0.0,0.0,0.0],[3.0,0.0,0.0,0.0]]
-trackList = Array.new(ratings.shape[1])
-numFeatures = 2
-lambda = 1
-g = ConfiCost.new(ratings, trackList, numFeatures, lambda)
-g.minCost
-puts "new theta"
-puts g.theta.to_a.to_s
-puts "new features"
-puts g.features.to_a.to_s
-puts "predictions"
-puts g.predictions.to_a.to_s
+##ratings = NArray.float(4,5).indgen(0,2)
+#ratings = NArray[[5.0,4.0,0.0,0.0],[3.0,0.0,0.0,0.0],[4.0,0.0,0.0,0.0],[3.0,0.0,0.0,0.0],[3.0,0.0,0.0,0.0]]
+#num_features = 2
+#lambda = 1
+#g = CofiCost.new(ratings, num_features, lambda)
+#puts g.theta.nil?
+#g.min_cost
+#puts "new theta"
+#puts g.theta.to_a.to_s
+#puts "new features"
+#puts g.features.to_a.to_s
+#puts "predictions"
+#puts g.predictions.to_a.to_s
